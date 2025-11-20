2930406c4be1c0883b3c4c4cb2b49b5366950401
VitalNya <124721465+VitalNya@users.noreply.github.com>
2025-11-20 14:32:23 -0500

    Update PALMER.sh
    Update

--- PALMER.sh
+++ PALMER.sh
@@ -1,15 +1,13 @@
-  GNU nano 2.9.8                                                                                                                                                                       PALMER_pipline.sh
-
 #!/bin/bash
 
 # ====================================================================================================
-# PALMER Pipline: Multi-sample, Multi-chromosome
+# PALMER Pipeline: Multi-sample, Multi-chromosome
 # Author: Vital Nyabashi
 # PALMER Credit: https://github.com/WeichenZhou/PALMER
 # Date: 11/07/2025
 #
 # Description:
-#       The piple runs PALMER on multiple long read BAM files across all the 22 autosomes (ch1-ch22)
+#       The pipeline runs PALMER on multiple long-read BAM files across all 22 autosomes (ch1-ch22)
 #       to detect NUMTS (nuclear mitochondrial DNA insertions).
 #
 # Output:
@@ -24,21 +22,20 @@
 #
 # Usage:
 #       - sbatch PALMER_pipline.sh
-#       - sbatch palmer_job.sh "$DATA_PATH" "$WORKDIR" "chr${CHR}"
+#       - sbatch PALMER_pipline_job.sh "$DATA_PATH" "$WORKDIR" "chr${CHR}"
 # ====================================================================================================
 
 
-#!/bin/bash
 #SBATCH --job-name=PALMER
 #SBATCH --mail-user=vitalnya@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --account=remills0
 #SBATCH --partition=standard
-#SBATCH --time=2:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4g
+#SBATCH --mem-per-cpu=30g
 
 # Accept arguments: data_path, workdir, chromosome
 DATA_PATH="$1"
@@ -51,11 +48,14 @@ mkdir -p "$WORKDIR"
 # Run PALMER
 /home/vitalnya/vitalnya/Mills_rotation/PALMER/PALMER \
   --input "$DATA_PATH" \
-  --workdir "$WORKDIR" \
+  --workdir "$WORKDIR/" \
   --ref_ver other \
-  --output "${WORKDIR}/output_${CHR}" \
+  --output "Output_${CHR}_numts" \
   --chr "$CHR" \
   --mode raw \
   --ref_fa /home/vitalnya/vitalnya/Mills_rotation/reference/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna \
   --type CUSTOMIZED \
   --custom_seq /home/vitalnya/vitalnya/Mills_rotation/reference/human_mitochondrion.fna
+
+# Remove files to free up space
+rm -rf "${WORKDIR}/${CHR}"*
